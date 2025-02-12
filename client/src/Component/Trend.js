5314d0411205c93d390388acec2e8a90a9a63ccf
Ajay Mehra <ajaymehra4492@gmail.com>
2025-02-12 22:43:58 +0530

    Solving some styling bugs

--- client/src/Component/Trend.js
+++ client/src/Component/Trend.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Trend = ({trend}) => {
   return (
-    <Box w={"100%"} p={"24px 4px"} paddingLeft={"40px"} cursor={'pointer'}>
+    <Box w={"100%"} padding={"24px 4px"} paddingLeft={"40px"} cursor={'pointer'} >
       <Text style={{fontWeight:"500",fontSize:"20px",letterSpacing:"2px"}}>{trend.name}</Text>
       <Text>{trend.posts} posts</Text>
 
