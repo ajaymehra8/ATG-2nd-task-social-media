2268bb10939770666c4d13a1d02ccefb3e48bb9e
Ajay Mehra <ajaymehra4492@gmail.com>
2025-02-12 21:56:30 +0530

    Initial commit

---
+++ server/utils/email.js
@@ -0,0 +1,44 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { convert } = require('html-to-text');
+
+module.exports = class Email {
+  constructor(user, token) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.token = token;
+    this.from = process.env.Email_Username;
+  }
+  newTransport() {
+    
+    return nodemailer.createTransport({
+      service: 'gmail',
+
+      auth: {
+        user: process.env.Email_Username,
+        pass: process.env.Email_Password,
+      },
+    });
+  }
+  async send(subject) {
+    // send the actual mail
+    
+    // 2) Define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html:`<h1>Your password reset otp is ${this.token}</h1>`,
+    };
+
+    // 3)Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('Welcome to the natours family');
+  }
+  async sendPasswordReset(){
+    await this.send('Your password reset token. (Valid only for 10 minutes)')
+  }
+};
